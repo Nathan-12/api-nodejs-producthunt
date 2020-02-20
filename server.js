4a2fdcfb160109561e45a54e93fee57dba9b460e
Nathan Lima <nathanlima.b@gmail.com>
2020-02-20 17:06:50 -0300

    Iniciando o Banco de dados

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o Banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser:true, useUnifiedTopology: true });
+
+//Crian a primeira rota
 app.get('/', function(req, res){
     res.send('Hello Nathan !!!');
 })
