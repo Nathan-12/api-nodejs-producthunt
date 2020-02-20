7e8fad8652a2974b592ef11949dbbc855747af5f
Nathan Lima <nathanlima.b@gmail.com>
2020-02-20 19:19:21 -0300

    Mudanças no server.js

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 //Iniciando o App
 const app = express();
 
 //Iniciando o Banco de dados
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/nodejs', 
+{ useNewUrlParser:true, useUnifiedTopology: true });
+
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 //Crian a primeira rota
-app.get('/', function(req, res){
-    res.send('Hello Nathan !!!');
+app.get('/', (req, res) =>{
+    
+    Product.create({
+        title: 'Nathan',
+        description: 'Github Nathan',
+        url: 'github.com/Nathan-12'
+    });
+    
+    return res.send('Hello Nathan !!!');
 })
 
 app.listen(3001);
